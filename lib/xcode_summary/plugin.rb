9c2a55a8771f6a10a12b82a22d4e2a74b297d8c6
Alex Moisei <dimentar@gmail.com>
2024-06-03 20:35:22 +0200

    Remove new lines

--- lib/xcode_summary/plugin.rb
+++ lib/xcode_summary/plugin.rb
@@ -337,6 +337,7 @@ module Danger
       # Substituting the pid for test retryies to filter them later.
       message = result.message
       message = message.sub(/, given input App element pid: \d{3,6}/, '.')
+      message = message.gsub(/\r\n$/, ' ').strip
       if message_length_limit >= 0
         message = message[0, message_length_limit]
         message += '...'
